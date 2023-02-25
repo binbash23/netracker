1820a45145d15e802cd1c7d56e3a1401d2d9a674
Jens Heine <binbash@gmx.net>
2023-02-25 20:22:24 +0100

    insert arpevent data

--- arpevent_tracker/source/openwrt/arpevent_tracker.sh
+++ arpevent_tracker/source/openwrt/arpevent_tracker.sh
@@ -4,20 +4,26 @@
 #
 #
 
+#
+# Variables
+# 
 DB_FILENAME='tracker.db'
+
+#
+# Main
+#
+echo "Running $0"
 
 cat /proc/net/arp | tail -n +2 | 
 while read -r line; do 
-  #echo "$line"
   line_array=($line)
-  #echo "Array:  ${line_array[@]}"
-  #echo "Array:  '${line_array[4]}'"
-  #eval "${line_array[4]}"
   sqlite3 ${DB_FILENAME} "pragma synchronous=0"
-
-  echo "insert into arpevent (UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${line_array[0]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','*','dummy')"
-  sqlcommand="insert into arpevent (UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${line_array[1]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','*','dummy')"
+  sqlcommand="insert into arpevent (UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${line_array[0]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','*','dummy')"
+  #echo "$sqlcommand"
+  echo -n .
   sqlite3 ${DB_FILENAME} "$sqlcommand"
 done
 
+echo 
+echo Done
 
