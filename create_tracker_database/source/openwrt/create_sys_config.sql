b1d31c6de569b8d737a2ab2e8f39c086af7db9e9
Jens Heine <binbash@gmx.net>
2023-02-25 23:40:10 +0100

    file deleted

--- create_tracker_database/source/openwrt/create_sys_config.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "sys_config" (
-        "PROPERTY"      TEXT,
-        "VALUE" TEXT,
-        PRIMARY KEY("PROPERTY")
-);
-insert or replace into sys_config (property, value) values ('OS', 'linux');
-
