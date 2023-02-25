8a31b3278b521733a17c85aaccc7e864f2046c28
Jens Heine <binbash@gmx.net>
2023-02-25 23:36:53 +0100

    create initial tracker db

--- create_tracker_database/source/openwrt/create_tracker_database.sh
+++ create_tracker_database/source/openwrt/create_tracker_database.sh
@@ -15,7 +15,6 @@
 #
 DB_FILENAME='tracker.db'
 DB_CREATE_SCRIPT_FILENAME='create_tracker_database.sql'
-DB_CREATE_SYS_CONFIG_SCRIPT_FILENAME='create_sys_config.sql'
 
 #
 # Main
@@ -25,12 +24,30 @@ echo "Running $0"
 echo "Checking netracker database..."
 sqlite3 ${DB_FILENAME} < ${DB_CREATE_SCRIPT_FILENAME}
 
-echo "Checking sys_config table..."
-sqlite3 ${DB_FILENAME} < ${DB_CREATE_SYS_CONFIG_SCRIPT_FILENAME}
+#echo "Checking sys_config table..."
+#sqlite3 ${DB_FILENAME} < ${DB_CREATE_SYS_CONFIG_SCRIPT_FILENAME}
+
+echo "Checking tracker database os..."
+ROW=`sqlite3 ${DB_FILENAME} "select * from sys_config where property='TRACKER_OS'"`
+if [ ! -z "${ROW}" ]; then
+  echo "TRACKER_OS exists."
+else
+  echo "TRACKER_OS is missing, inserting new TRACKER_OS..."
+  sqlite3 ${DB_FILENAME} "insert into sys_config (PROPERTY, VALUE) values ('TRACKER_OS', 'linux')"
+fi
+
+echo "Checking tracker database create_date..."
+ROW=`sqlite3 ${DB_FILENAME} "select * from sys_config where property='TRACKER_DATABASE_CREATE_DATE'"`
+if [ ! -z "${ROW}" ]; then
+  echo "TRACKER_DATABASE_CREATE_DATE exists."
+else
+  echo "TRACKER_DATABASE_CREATE_DATE is missing, inserting new TRACKER_DATABASE_CREATE_DATE..."
+  sqlite3 ${DB_FILENAME} "insert into sys_config (PROPERTY, VALUE) values ('TRACKER_DATABASE_CREATE_DATE', datetime(CURRENT_TIMESTAMP, 'localtime'))"
+fi
 
 echo "Checking tracker UUID..."
 UUID=`sqlite3 ${DB_FILENAME} "select * from sys_config where property='TRACKER_UUID'"`
-if "${UUID}"; then
+if [ ! -z "${UUID}" ]; then
   echo "UUID exists."
 else
   echo "UUID is missing, inserting new UUID..."
