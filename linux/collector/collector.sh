ca99b3f63c0d0509d9ebe21d750e3abdefb1ef13
binbash23 <binbash@gmx.net>
2023-02-26 20:58:56 +0100

    collector und collector.library first version

--- linux/collector/collector.sh
+++ linux/collector/collector.sh
@@ -3,3 +3,22 @@
 #
 # 20230226 written by jens heine <binbash@gmx.net>, andreas stöcker <a.stoecker@gmx.net>
 #
+
+#set -x
+
+#
+# Source main library
+#
+. collector.library
+
+#
+# Main
+#
+echo "Running $0"
+
+[ -z "${DATABASE_FILENAME}" ] && { echo "Database filename not set."; exit 1; }
+
+echo "Checking database ${DATABASE_FILENAME}..."
+./create_tracker_database.sh
+
+echo $0 finished.
