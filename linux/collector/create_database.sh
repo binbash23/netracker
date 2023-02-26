53d03e294a631b3264f47983a67b745c0cf0fd7e
binbash23 <binbash@gmx.net>
2023-02-26 21:41:00 +0100

    Clean infos when checking database

--- linux/collector/create_database.sh
+++ linux/collector/create_database.sh
@@ -29,32 +29,35 @@ echo "Running $0"
 
 [ -z "${DATABASE_FILENAME}" ] && { echo "Database filename not set."; exit 1; }
 
-echo "Checking database..."
+echo "Checking/creating database structure..."
 sqlite3 ${DATABASE_FILENAME} < ${DB_CREATE_SCRIPT_FILENAME}
 
-echo "Checking sys_config property: OS..."
+echo -n "Checking sys_config property: OS..."
 ROW=`sqlite3 ${DATABASE_FILENAME} "select * from sys_config where property='OS'"`
 if [ ! -z "${ROW}" ]; then
-  echo "Property OS exists."
+  echo " OK"
 else
+  echo
   echo "Creating OS property..."
   sqlite3 ${DATABASE_FILENAME} "insert into sys_config (PROPERTY, VALUE) values ('OS', 'linux')"
 fi
 
-echo "Checking sys_config property: DATABASE_CREATE_DATE..."
+echo -n "Checking sys_config property: DATABASE_CREATE_DATE..."
 ROW=`sqlite3 ${DATABASE_FILENAME} "select * from sys_config where property='DATABASE_CREATE_DATE'"`
 if [ ! -z "${ROW}" ]; then
-  echo "DATABASE_CREATE_DATE exists."
+  echo " OK"
 else
+  echo
   echo "DATABASE_CREATE_DATE is missing, inserting new DATABASE_CREATE_DATE..."
   sqlite3 ${DATABASE_FILENAME} "insert into sys_config (PROPERTY, VALUE) values ('DATABASE_CREATE_DATE', datetime(CURRENT_TIMESTAMP, 'localtime'))"
 fi
 
-echo "Checking sys_config property: DATABASE_UUID..."
+echo -n "Checking sys_config property: DATABASE_UUID..."
 UUID=`sqlite3 ${DATABASE_FILENAME} "select * from sys_config where property='DATABASE_UUID'"`
 if [ ! -z "${UUID}" ]; then
-  echo "DATABASE_UUID exists."
+  echo " OK"
 else
+  echo
   echo "DATABASE_UUID is missing, inserting new DATABASE_UUID..."
   UUID=`uuidgen`
 sqlite3 ${DATABASE_FILENAME} "insert into sys_config (PROPERTY, VALUE) values ('DATABASE_UUID', '${UUID}')"
