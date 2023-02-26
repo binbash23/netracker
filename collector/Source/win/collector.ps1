0569cff213a867ab6800c5d7520d58982b6a9c79
andiecodes <morganh@gmx.li>
2023-02-26 15:38:15 +0100

    ...

---
+++ collector/Source/win/collector.ps1
@@ -0,0 +1,17 @@
+<#
+    Author: Jens Heine & Andreas Stöcker
+    Purpose: the collector script takes responsibility of the executed tracker, 
+             it also takes care of the Main Database   
+#>
+
+#region Dependencies
+
+#endregion Dependencies
+
+#region Config
+
+#endregion Config
+
+#region Main 
+
+#endregion Main
