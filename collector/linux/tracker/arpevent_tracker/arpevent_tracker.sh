572cb07237adca7695fcddfd235e501bd8951a5f
binbash23 <binbash@gmx.net>
2023-02-27 14:57:53 +0100

    tracker specific tables will be created by every tracker himself

--- collector/linux/tracker/arpevent_tracker/arpevent_tracker.sh
+++ collector/linux/tracker/arpevent_tracker/arpevent_tracker.sh
@@ -13,15 +13,25 @@
 # VARIABLES
 # 
 DATABASE_FILENAME="../../${DATABASE_FILENAME}"
+DB_CREATE_SCRIPT_FILENAME="create_table_arpevent.sql"
 
 #
 # MAIN
 #
 log $0 "Running $0"
 
+if [ -z "${1}" ]; then
+  COLLECTION_UUID=`uuidgen`
+else
+  COLLECTION_UUID="${1}"
+fi
+  
+# Create arpevent table if not exists
+sqlite3 ${DATABASE_FILENAME} < "$DB_CREATE_SCRIPT_FILENAME"
+
 while read -a line_array; do 
   #sqlite3 ${DATABASE_FILENAME} "pragma synchronous=0"
-  sqlcommand="insert into arpevent (UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${line_array[0]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','${line_array[4]}','${line_array[5]}')"
+  sqlcommand="insert into arpevent (UUID, COLLECTION_UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${COLLECTION_UUID}', '${line_array[0]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','${line_array[4]}','${line_array[5]}')"
   log $0 "Found arp entry IP:${line_array[0]}, MAC: ${line_array[3]}" 4
   sqlite3 ${DATABASE_FILENAME} "$sqlcommand"
 done < <(tail -n +2 /proc/net/arp)
---
+++ collector/linux/tracker/arpevent_tracker/create_table_arpevent.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "arpevent" (
+  "UUID"  TEXT,
+  "COLLECTION_UUID"  TEXT,
+  "CREATE_DATE" datetime not null default (datetime(CURRENT_TIMESTAMP, 'localtime')),
+  "IP"  TEXT,
+  "HW_TYPE" TEXT,
+  "FLAGS" TEXT,
+  "HW_ADDRESS"  TEXT,
+  "MASK"  TEXT,
+  "DEVICE"  TEXT,
+  PRIMARY KEY("UUID")
+);
+
