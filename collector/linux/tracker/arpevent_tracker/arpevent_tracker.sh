a77b37b2c8d4be5d9fd1c6497312e6469571096d
binbash23 <binbash@gmx.net>
2023-02-27 14:15:37 +0100

    realtive path problems fixed

--- collector/linux/tracker/arpevent_tracker/arpevent_tracker.sh
+++ collector/linux/tracker/arpevent_tracker/arpevent_tracker.sh
@@ -5,25 +5,33 @@
 #
 
 #
-# Variables
-# 
-DB_FILENAME='tracker.db'
+# SOURCE LIBRARIES
+#
+. ../../collector.library
 
 #
-# Main
+# VARIABLES
+# 
+DATABASE_FILENAME="../../${DATABASE_FILENAME}"
+
 #
-echo "Running $0"
+# MAIN
+#
+log $0 "Running $0"
 
 cat /proc/net/arp | tail -n +2 | 
 while read -r line; do 
   line_array=($line)
-  sqlite3 ${DB_FILENAME} "pragma synchronous=0"
+  #sqlite3 ${DATABASE_FILENAME} "pragma synchronous=0"
   sqlcommand="insert into arpevent (UUID, IP, HW_TYPE, FLAGS, HW_ADDRESS, MASK, DEVICE) values ('`uuidgen`', '${line_array[0]}', '${line_array[1]}','${line_array[2]}','${line_array[3]}','*','dummy')"
   #echo "$sqlcommand"
-  echo -n .
-  sqlite3 ${DB_FILENAME} "$sqlcommand"
+  #echo -n .
+  #echo ${line_array[@]}
+  log $0 "Found arp entry IP:${line_array[0]}, MAC: ${line_array[3]}" 4
+  sqlite3 ${DATABASE_FILENAME} "$sqlcommand"
 done
 
-echo 
-echo Done
+log $0 "Finished $0"
+
+exit 0
 
