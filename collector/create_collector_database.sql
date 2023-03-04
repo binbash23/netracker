5aafa687bc81e45f0240d8232265f33cff058bc5
binbash23 <binbash@gmx.net>
2023-03-04 14:33:12 +0100

    tracker_config table

--- collector/create_collector_database.sql
+++ collector/create_collector_database.sql
@@ -1,15 +1,21 @@
 CREATE TABLE IF NOT EXISTS "sys_config" (
         "CREATE_DATE" datetime not null default (datetime(CURRENT_TIMESTAMP, 'localtime')),
-        "PROPERTY"      TEXT,
-        "VALUE" TEXT,
+        "PROPERTY"    TEXT,
+        "VALUE"       TEXT,
         PRIMARY KEY("PROPERTY")
 );
-CREATE TABLE IF NOT EXISTS "log" (
-        "ID"      INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS "tracker_config" (
         "CREATE_DATE" datetime not null default (datetime(CURRENT_TIMESTAMP, 'localtime')),
-        "LOG_LEVEL" TEXT,
-        "MESSAGE" TEXT not null,
-        "SOURCE" TEXT,
+        "NAME"        TEXT,
+        "ENABLED"     TEXT,
+        PRIMARY KEY("NAME")
+);
+CREATE TABLE IF NOT EXISTS "log" (
+        "ID"          INTEGER PRIMARY KEY AUTOINCREMENT,
+        "CREATE_DATE" datetime not null default (datetime(CURRENT_TIMESTAMP, 'localtime')),
+        "LOG_LEVEL"   TEXT,
+        "MESSAGE"     TEXT not null,
+        "SOURCE"      TEXT,
         "COLLECTION_UUID" TEXT
 );
 
