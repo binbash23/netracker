b1489a959e13b052e486a84a9bdf75e415c10fff
binbash23 <binbash@gmx.net>
2023-03-06 13:02:22 +0100

    default heartbeat to 5. hostevent tracker scans all available networks now.

--- collector/linux/tracker/hostevent_tracker/hostevent_tracker.sh
+++ collector/linux/tracker/hostevent_tracker/hostevent_tracker.sh
@@ -29,11 +29,19 @@ log $0 "Running $0" 3 ${COLLECTION_UUID}
 # Create neccessary table(s) if not exist
 sqlite3 ${DATABASE_FILENAME} < "$DB_CREATE_SCRIPT_FILENAME"
 
-while read -a line_array; do 
-  sqlcommand="insert into t_hostevent (UUID, COLLECTION_UUID, IP, DNS_NAME, STATUS) values ('`uuidgen`', '${COLLECTION_UUID}', '${line_array[1]}', '${line_array[2]}','${line_array[4]}')"
-  log $0 "Found host entry IP:${line_array[0]}, DNS_NAME: ${line_array[1]}" 4 ${COLLECTION_UUID}
-  sqlite3 ${DATABASE_FILENAME} "$sqlcommand"
-done < <(nmap 192.168.1.0/24 -sn -oG -|grep -v '#')
+# collect available networks from t_netevent table if exists
+available_networks_array=(`sqlite3 ${DATABASE_FILENAME} "select distinct ipv4 from t_netevent where STATUS='UP' and CREATE_DATE=(select max(CREATE_DATE) from t_netevent)"`)
+
+log $0 "Available networks to scan: ${available_networks_array[@]}"
+
+
+for current_network in "${available_networks_array[@]}"; do
+  while read -a line_array; do 
+    sqlcommand="insert into t_hostevent (UUID, COLLECTION_UUID, IP, DNS_NAME, STATUS) values ('`uuidgen`', '${COLLECTION_UUID}', '${line_array[1]}', '${line_array[2]}','${line_array[4]}')"
+    log $0 "Found host entry IP:${line_array[0]}, DNS_NAME: ${line_array[1]}" 4 ${COLLECTION_UUID}
+    sqlite3 ${DATABASE_FILENAME} "$sqlcommand"
+  done < <(nmap "${current_network}" -sn -oG -|grep -v '#')
+done
 
 log $0 "Finished $0" 3 ${COLLECTION_UUID}
 
